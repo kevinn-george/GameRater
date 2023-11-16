04c009b0d7ae1f2c454656d7188c9e510e9a694b
kevin_george <keving2549@gmail.com>
2023-11-16 22:02:54 +0400

    Login Page Setup

---
+++ src/gamerater/LoginUser.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamerater;
+
+/**
+ *
+ * @author wissam
+ */
+public class LoginUser {
+    String Email;
+    String Name; 
+    int type;
+    
+    LoginUser(String uemail, String uname, int utype) {
+        Email = uemail;
+        Name = uname;
+        type = utype;
+     //   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
